5d4209a1b939eb1266a20a7fe3c4feb803eb3aea
John Coleman <john65@pobox.com>
2012-01-08 11:47:23 -0500

    clean up samples for testing

--- test.mk
+++ test.mk
@@ -1,13 +1,9 @@
 all: sample aper
 
 sample:
-	time awk -f aper.awk sample_list.txt > old
-	time ./aper.sh sample_list.txt > new
-	wc -l old new
-	diff -i -w old new | grep -v whitelist@example.com
+	time ./aper.sh sample_list.txt > aper.sample
+	wc -l aper.sample
 aper:
-	time awk -f aper.awk phishing_reply_addresses > old2
-	time ./aper.sh phishing_reply_addresses > new2
-	wc -l old2 new2
-	diff -i -w old2 new2 | grep -v whitelist@example.com
+	time ./aper.sh phishing_reply_addresses > aper
+	wc -l aper
 
